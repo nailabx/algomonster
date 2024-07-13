65dddf2570f5bf0bc25913e8031b6adc64ae67cd
nailabx <zinalacina@nailabx.com>
2024-07-13 15:50:07 -0500

    Robot collisions

---
+++ utils/trie_node.py
@@ -0,0 +1,21 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None]*26
+        self.isEndOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def charToIndex(self, ch):
+        # Convert character to index (0-25)
+        return ord(ch) - ord('a')
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            index = self.charToIndex(char)
+            if not node.children[index]:
+                node.children[index] = TrieNode()
+            node = node.children[index]
+        node.isEndOfWord = True
